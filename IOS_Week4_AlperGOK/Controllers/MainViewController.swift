2c7165f2bef57ad590de48817a6499ca8f1cce75
alpergok <alpergok92@outlook.com>
2021-10-13 16:01:24 +0300

    navigationBar bg color added

--- IOS_Week4_AlperGOK/Controllers/MainViewController.swift
+++ IOS_Week4_AlperGOK/Controllers/MainViewController.swift
@@ -14,6 +14,7 @@ class MainViewController: UIViewController {
     var characterManager = CharacterManager()
     
     override func viewDidLoad() {
+        navigationController?.navigationBar.backgroundColor = .gray
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
